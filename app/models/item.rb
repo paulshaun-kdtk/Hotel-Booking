9853cbf51adc63f5a06a849c362096b58b616a26
Muhammad Zunair khan <zunairkhan811@gmail.com>
2024-01-19 17:16:12 +0500

    install rspec and add test for item model and fix linter errors

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-    has_one :reservation
-    belongs_to :user, through: :reservation
-    validates :name, :description, :image, presence: true
-    validates :finance_fee, :purchase_fee, :total_amount, :duration, :apr, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  has_one :reservation
+  has_one :user, through: :reservation
+  validates :name, :description, :image, presence: true
+  validates :finance_fee, :purchase_fee, :total_amount, :duration, :apr, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
