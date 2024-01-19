506d462119562708ea91d6d9c4a1d4f7188026fc
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-01-19 11:15:32 +0200

    Create a Reservation model, validations and specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_181753) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_19_090258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "date"
+    t.string "city"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -29,4 +38,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_181753) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "users"
 end
