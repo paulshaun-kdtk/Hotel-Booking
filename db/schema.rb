741c455dc20c29d224bc0c93fe8e48f8af6d5e91
Muhammad Zunair khan <zunairkhan811@gmail.com>
2024-01-19 16:39:31 +0500

    do migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_181753) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_19_101448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "image"
+    t.decimal "finance_fee", precision: 10, scale: 2
+    t.decimal "purchase_fee", precision: 10, scale: 2
+    t.decimal "total_amount", precision: 10, scale: 2
+    t.integer "duration"
+    t.decimal "apr", precision: 5, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
