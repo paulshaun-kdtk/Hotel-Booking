1d3de87bd17071a45add3ad3ac3a7b6cfe3722cb
Muhammad Zunair khan <zunairkhan811@gmail.com>
2024-01-22 12:27:00 +0500

    make changes to migration files and database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_19_101448) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_19_090258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_19_101448) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jti", null: false
