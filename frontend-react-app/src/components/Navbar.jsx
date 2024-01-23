005805ea85ec979e19135656eff147f0e6e53ca8
mumbereh <siirasonh@gmail.com>
2024-01-23 19:06:44 +0300

    Add Navbar styling

--- frontend-react-app/src/components/Navbar.jsx
+++ frontend-react-app/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
+
 const Navbar = () => {
   const location = useLocation();
   const isActive = (pathname) => location.pathname === pathname;
