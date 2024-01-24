1df2e3e9e25f45eb0fa6e9e57c73d4733f59e2a4
[Megagig] <[megagigsoftwaresolution@gmail.com]>
2024-01-24 12:18:42 +0100

    Create the itemActions

---
+++ frontend-react-app/src/components/redux/actions/itemActions.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const FETCH_ITEM_DETAILS_SUCCESS = 'FETCH_ITEM_DETAILS_SUCCESS';
+
+const fetchItemDetailsSuccess = (item) => ({
+  type: FETCH_ITEM_DETAILS_SUCCESS,
+  payload: item,
+});
+
+const fetchItemDetails = (itemId) => {
+  return (dispatch) => {
+    axios
+      .get(`/api/v1/items/${itemId}`)
+      .then((response) => {
+        dispatch(fetchItemDetailsSuccess(response.data));
+      })
+      .catch((error) => {
+        console.error('Error during item details fetching:', error);
+      });
+  };
+};
+
+export default fetchItemDetails;
