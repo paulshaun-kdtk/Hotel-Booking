a02d352bbb2669e03aafd2073e586855a74c1321
Muhammad Zunair khan <zunairkhan811@gmail.com>
2024-01-31 18:29:05 +0500

    make executeable render-build.sh file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,6 @@
 # exit on error
 set -o errexit
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
